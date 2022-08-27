ab2a6c7983e94b697fd1a9e5a92a9a8ddd89472f
Archimedes Li <escaperecovery@gmail.com>
2022-08-27 00:51:42 +0000

    Assembly

--- server.js
+++ server.js
@@ -30,24 +30,57 @@ const assembly = axios.create({
     },
 });
 
-async function queueAudio(url)
-  return new Promise(()=>{
+function queueAudio(url) {
+  return new Promise(resolve=>{
     assembly.post("/transcript", {
           audio_url: url
       })
-      .then((res) => {resolve();})
+      .then((res) => {resolve(res.data.id);})
       .catch((err) => console.error(err));
-  }
+  });
 }
 
-async function getAudio(id) {
-  assembly
-    .get(`/transcript/${id}`)
-    .then((res) => console.log(res.data))
-    .catch((err) => console.error(err));
-}
-
-var a = queueAudio("https://bit.ly/3yxKEIY");
-setInterval(()=>{
+function getTranscript(id) {
+  return new Promise(resolve => {
+    var n = setInterval(() => {
+      assembly
+      .get(`/transcript/${id}`)
+      .then((res) => {
+        if (res.data.status == "completed") {
+          clearInterval(n);//stop checking
+          resolve(res.data.text)
+        }
+      })
+      .catch((err) => console.error(err));
+    }, 10000);
+    
+  });
   
-}, 10000)
+}
+
+async function convertAssembly(url) {
+  var id = await queueAudio(url);
+  console.log("Request sent with id: " + id)
+  var t = await getTranscript(id)
+  console.log(t)
+  return t;
+}
+//example pull
+convertAssembly("https://bit.ly/3yxKEIY")
+
+function resolveAfter2Seconds() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve('resolved');
+    }, 2000);
+  });
+}
+
+async function asyncCall() {
+  console.log('calling');
+  const result = await resolveAfter2Seconds();
+  console.log(result);
+  // expected output: "resolved"
+}
+
+//asyncCall();
