5fd17e84ce833712a34e12638ea72c5e787bc0e2
theamazing0 <66532288+theamazing0@users.noreply.github.com>
2022-08-27 14:10:12 -0400

    Update secrets.js

--- secrets.js
+++ secrets.js
@@ -1,5 +1,12 @@
 // Add Your API Keys Here
 
-// AssemblyAI API Key
+// -- AssemblyAI API Key
+
+// Normal Way
+// const assemblyAiKey = "INSERT ASSEMBLY AI KEY HERE"
+
+// Replit Way
 const assemblyAiKey = process.env['assembly']
+
+
 module.exports = {assemblyAiKey}
