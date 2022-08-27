815c20226af0e181b957c8dbd98f2ff2671fd722
theAmazingByte <66532288+theamazing0@users.noreply.github.com>
2022-08-27 15:00:26 +0000

    Some Changes To Frontend

--- client/main.js
+++ client/main.js
@@ -1,11 +1,15 @@
-/*(document).ready(function () {
-	
-})*/
+$(document).ready(function() {
+	$("#select-video-card").hide();
+	$("#open-select-video").click(function() {
+		$("#select-video-card").show();
+		$("#start-card").hide();
+	});
+});
 // //////////////////
 // fetch("/trans", {
 //   method: "POST",  
 //   headers: {
-    
+
 //   },
 //   body: JSON.stringify()
 // }).then((res)=>res.text())
@@ -18,14 +22,14 @@
 // fetch("/trans", {
 //   method: "POST",  
 //   headers: {
-    
+
 //   },
 //   body: JSON.stringify()
 // }).then((res)=>res.text())
 // .then((data) => {
 
 
-  
+
 // })
-  
-// })
+
+//})
